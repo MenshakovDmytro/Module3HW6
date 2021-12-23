e2ab2b494ebcce5051b9d55fa29e2cf23893f0b2
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-23 18:59:14 +0200

    added MessageBox

---
+++ Module3HW6/Module3HW6/MessageBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Module3HW6
+{
+    public class MessageBox
+    {
+        public event Action<State> OnClosed;
+
+        public async void Open()
+        {
+            Console.WriteLine("Window is open");
+            await Task.Delay(3000);
+            Console.WriteLine("Window is closed");
+            var random = new Random().Next(2);
+            OnClosed.Invoke((State)random);
+        }
+    }
+}
